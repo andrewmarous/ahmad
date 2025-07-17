fc2fe490870d40cff5cf5837fbc7278fc280af01
Andrew Marous <andrew.marous@orendapower.com>
2025-07-17 12:47:07 -0400

    fix: user request will properly stream file now

--- treble/src/agent.rs
+++ treble/src/agent.rs
@@ -1,21 +1,44 @@
 use std::env;
 
+use futures::StreamExt;
 use iced_futures::stream::try_channel;
+use iced_futures::core::image::Bytes;
 use futures::{stream::Stream, SinkExt};
+use reqwest::Client;
+use url::Url;
+use tokio::io::{self, AsyncWriteExt};
+use tokio::fs::File;
+use serde::{Serialize, Deserialize};
 
 use anyhow::Error;
 
 use std::{
-    fs, io::{Read, Write}, net::{TcpListener, TcpStream}, path::PathBuf
+    fs, net::TcpStream, path::PathBuf
 };
 
-pub fn check_backend() -> impl Stream<Item= Result<(), Error>> {
-    let port_check = async move {
-        let port: u16 = 8000;
-        let url: String = env::var("API_URL").expect("API_URL must be defined");
-        let url: &str = &url[..];
+#[derive(Debug, Serialize)]
+struct GenerationPayload<'a> {
+    prompt: &'a str,
+    negative_prompt: &'a str,
+    #[serde(rename = "client_output_path")]
+    filename: &'a str,
+}
 
-        TcpListener::bind((url, port))?;
+#[derive(Debug, Deserialize)]
+struct GenerationResponse {
+}
+
+fn api_url(endpoint: &str) -> Url {
+    let mut url_base: String = env::var("API_URL").expect("API_URL must be defined");
+    url_base.push_str(endpoint);
+    Url::parse(&url_base).expect("Given url is not valid.")
+}
+
+pub fn check_backend() -> impl Stream<Item= Result<(), Error>> {
+    let port: u16 = 8000;
+    let url: String = env::var("API_URL").expect("API_URL must be defined");
+    let port_check = async move {
+        TcpStream::connect((url, port))?;
         Ok(())
     };
     futures::stream::once(port_check)
@@ -24,24 +47,34 @@ pub fn check_backend() -> impl Stream<Item= Result<(), Error>> {
 pub fn request_response_stream(prompt: String, output_path: PathBuf) -> impl Stream<Item= Result<String, Error>> {
     try_channel(
         1, move |mut sender| async move {
-            let mut stream = TcpStream::connect("127.0.0.1:8000")?;
-            sender.send(String::from("25.0")).await?;
+            sender.send(String::from("33.0")).await?;
 
-            let request = "";
+            let client = Client::new();
+            let payload = GenerationPayload {
+                prompt: &prompt[..],
+                negative_prompt: "Low quality, average quality".into(),
+                filename: "",
+            };
+            sender.send(String::from("66.0")).await?;
 
-            stream
-                .write_all(prompt.as_bytes())
-                .and_then(|_| stream.write_all(b"\n"))?;
-            sender.send(String::from("50.0")).await?;
+            let response = client
+                .post(api_url("generate"))
+                .json(&payload)
+                .send()
+                .await?;
+            sender.send(String::from("99.0")).await?;
 
-            let mut buf = Vec::new();
-            stream
-                .read_to_end(&mut buf)?;
-            let response = String::from_utf8(buf)?;
-            sender.send(String::from("75.0")).await?;
+            let mut file = File::create(&output_path).await?;
+            let mut downloaded = 0usize;
 
-            fs::write(output_path, &response)?;
-            sender.send(response).await?;
+            let mut bs = response.bytes_stream();
+            while let Some(chunk) = bs.next().await {
+                let written = file.write(chunk
+                    .unwrap_or(Bytes::new())
+                    .as_ref()).await?;
+                downloaded += written;
+            }
+            sender.send(String::from(downloaded.to_string())).await?;
 
             Ok(())
         }
