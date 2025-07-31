7d4bb55d6c3ad96c9ac0b0e79366f3f4710e423d
Andrew Marous <andrew.marous@orendapower.com>
2025-07-31 09:26:01 -0400

    fix: updated agent requests to work with RunPod API

--- treble/src/agent.rs
+++ treble/src/agent.rs
@@ -10,6 +10,8 @@ use url::Url;
 use tokio::io::{self, AsyncWriteExt};
 use tokio::fs::File;
 use serde::{Serialize, Deserialize};
+use serde_json::json;
+use bytes::Bytes;
 
 use anyhow::Error;
 
@@ -22,6 +24,24 @@ use std::{
 use crate::TaskResponse;
 
 #[derive(Debug, Serialize)]
+struct RunpodRequest<'a> {
+    endpoint: &'a str,
+    payload: Payload<'a>
+}
+
+#[derive(Debug, Serialize)]
+#[serde(untagged)]
+enum Payload<'a> {
+    Generate(GenerationPayload<'a>),
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(untagged)]
+enum Response {
+    Generate(GenerationResponse),
+}
+
+#[derive(Debug, Serialize)]
 struct GenerationPayload<'a> {
     prompt: &'a str,
     negative_prompt: &'a str,
@@ -31,6 +51,10 @@ struct GenerationPayload<'a> {
 
 #[derive(Debug, Deserialize)]
 struct GenerationResponse {
+    file_data: String,
+    file_name: String,
+    media_type: String,
+    status_code: i16,
 }
 
 fn api_url(endpoint: &str) -> Result<Url, Error> {
@@ -70,8 +94,13 @@ pub fn request_response_stream(
             let headers = {
                 let mut res = HeaderMap::new();
                 let k = "keep-alive";
+                let key = format!("Bearer {}", env!("RUNPOD_API_KEY"));
                 res.append(CONNECTION, HeaderValue::from_static(k));
                 res.append(HeaderName::from_static(k), HeaderValue::from_static("timeout=300, max=50"));
+                res.append(
+                    HeaderName::from_static("Authorization"),
+                    HeaderValue::from_str(&key[..]).expect("Format of API key is invalid."),
+                );
                 res
             };
             let Some(filetype) = output_path.extension() else {
@@ -80,15 +109,18 @@ pub fn request_response_stream(
                         .midi or .wav"))).await?;
                 return Ok(());
             };
-            let payload = GenerationPayload {
-                prompt: &prompt[..],
-                negative_prompt: "Low quality, average quality".into(),
-                filetype: filetype.to_str().unwrap()
+            let payload = RunpodRequest {
+                endpoint: "generate",
+                payload: Payload::Generate(GenerationPayload {
+                    prompt: &prompt[..],
+                    negative_prompt: "Low or medium quality",
+                    filetype: &filetype.to_str().unwrap()
+                })
             };
             info!("built request payload.");
             sender.send(TaskResponse::Progress(66.0)).await?;
 
-            let response = client.post(api_url("/generate").expect("Given endpoint is invalid."))
+            let response = client.post(api_url("/run").expect("Given endpoint is invalid."))
                 .headers(headers)
                 .json(&payload)
                 .send()
@@ -98,13 +130,14 @@ pub fn request_response_stream(
             sender.send(TaskResponse::Progress(99.0)).await?;
 
             info!("building file...");
-            let bytes = response.bytes().await?;
-            let len = bytes.len();
+            let response = response.json::<GenerationResponse>().await?;
+            let bytes: Bytes = response.file_data.into_bytes().into();
+
 
             fs::write(output_path, bytes)?;
             info!("response file successfully built.");
             sender.send(TaskResponse::String(String::from(
-                format!("{}", len)
+                format!("{}", bytes.len())
             ))).await?;
 
             Ok(())
